71284431627e14c07e72200414fe64ceeec63c88
Kevin <macbook@macbooks-MacBook-Air.local>
2020-06-29 19:31:07 +0200

    Updates

---
+++ precache-manifest.8ca83224675fdd1e05172c780b18b521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7771b8e1cceeb74c3ffced4840db5e66",
+    "url": "/eventmanager/index.html"
+  },
+  {
+    "revision": "53bee8f8e10f7e8640dc",
+    "url": "/eventmanager/static/css/2.aee84c9b.chunk.css"
+  },
+  {
+    "revision": "53bee8f8e10f7e8640dc",
+    "url": "/eventmanager/static/js/2.b9ac656c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/eventmanager/static/js/2.b9ac656c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d275ed2057343ad8a8e",
+    "url": "/eventmanager/static/js/main.69663736.chunk.js"
+  },
+  {
+    "revision": "a578e3a7df5bd1c1b9a7",
+    "url": "/eventmanager/static/js/runtime-main.ff2f4bde.js"
+  }
+]);
